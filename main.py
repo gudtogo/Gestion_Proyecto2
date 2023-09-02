a4ac505cac5c744e6443bdce268e10da570a341a
gudtogo <earias_1995@hotmail.com>
2023-09-02 15:00:37 -0400

    Estructura basica del codigo

---
+++ main.py
@@ -0,0 +1,9 @@
+from ludo.models.board import Board
+from ludo.models.player import Player
+from ludo.models.token import Token
+
+def main():
+    game_board = Board()
+
+if __name__ == "__main__":
+    main()
